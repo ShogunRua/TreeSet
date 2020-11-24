e87ba22536edbfd64bc7ff6f9125dd26b3c7cfe4
ShogunRua <70099118+ShogunRua@users.noreply.github.com>
2020-11-24 20:35:47 +0300

    TreeSet

---
+++ src/test/java/CarCollectionTest.java
@@ -0,0 +1,34 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CarCollectionTest {
+
+    private CarCollection carCollection;
+
+    @Before
+    public void setUp() throws Exception {
+        carCollection = new CarHashSet();
+        for (int i = 0; i < 100; i++) {
+            carCollection.add(new Car("Brand" + i, i));
+        }
+
+    }
+
+    @Test
+    public void contains(){
+        assertTrue(carCollection.contains(new Car("Brand 20", 20)));
+        assertFalse(carCollection.contains(new Car("Brand 200", 20)));
+
+    }
+
+    @Test
+    public void testForEach(){
+        int index = 0;
+        for (Car car : carCollection){
+            index++;
+        }
+        assertEquals(100, index);
+    }
+}
